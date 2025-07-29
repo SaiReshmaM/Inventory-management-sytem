b1be5dbd92f46442bcd2c84cdc74ce2d069b35ee
SaiReshmaM <157406677+SaiReshmaM@users.noreply.github.com>
2025-07-30 01:08:08 +0530

    Delete InventoryApp directory

--- InventoryApp/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from django import forms
-
-class RoleSelectionForm(forms.Form):
-    ROLES = (
-        ('admin', 'Admin'),
-        ('cashier', 'Cashier'),
-    )
-    role = forms.ChoiceField(choices=ROLES, widget=forms.RadioSelect)
